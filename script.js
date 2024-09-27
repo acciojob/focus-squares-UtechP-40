005e5cb680fd8cf4170c75505faf4fe89691bdde
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-27 23:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+for(let t of squares){
+    t.addEventListener("mouseover",(e)=>{
+        for(let t of squares){
+            if(e.target !=t){
+                t.style.backgroundColor = "#6F4E37"
+            }
+        }
+    })
+    t.addEventListener("mouseout",(e)=>{
+        for(let t of squares){
+            if(e.target !=t){
+                t.style.backgroundColor = "#E6E6FA"
+            }
+        }
+    })
+}
